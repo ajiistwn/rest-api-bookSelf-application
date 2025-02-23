8347844a02877f49169b0fb79d1b1b3d90e82c9d
Aji Setiawan <ajiisetiawan09@gmail.com>
2025-02-23 08:29:07 +0700

    try to cloud

--- src/server.js
+++ src/server.js
@@ -1,19 +1,19 @@
 const Hapi = require('@hapi/hapi');
 const routes = require('./routes')
- 
+
 const init = async () => {
     const server = Hapi.server({
-        port: 9000,
-        host: 'localhost',
+        port: 80,
+        host: '202.10.44.99',
         routes: {
             cors: true,
         },
     });
 
     server.route(routes);
-    
+
     await server.start();
     console.log(`Server berjalan pada ${server.info.uri}`);
 };
- 
+
 init();
